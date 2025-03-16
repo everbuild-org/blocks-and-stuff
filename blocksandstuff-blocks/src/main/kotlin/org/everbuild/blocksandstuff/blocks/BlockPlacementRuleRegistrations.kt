09f77fe7e2bad930320b1b3b7f94e5705d7c8af2
Christian Bergschneider <christian.bergschneider@gmx.de>
2025-03-16 17:17:06 +0100

    feat: logging for block placement rule registration
    Introduced a logger to track block placement rule registrations. Counts and logs the total number of rules registered to improve visibility and debugging during runtime.

--- blocksandstuff-blocks/src/main/kotlin/org/everbuild/blocksandstuff/blocks/BlockPlacementRuleRegistrations.kt
+++ blocksandstuff-blocks/src/main/kotlin/org/everbuild/blocksandstuff/blocks/BlockPlacementRuleRegistrations.kt
@@ -5,6 +5,8 @@ import org.everbuild.blocksandstuff.blocks.group.VanillaPlacementRules
 import org.everbuild.blocksandstuff.blocks.group.placement.PlacementGroup
 
 object BlockPlacementRuleRegistrations {
+    private val logger = MinecraftServer.LOGGER
+
     @JvmStatic
     fun registerDefault() {
         register(*VanillaPlacementRules.ALL.toTypedArray<PlacementGroup>())
@@ -13,12 +15,16 @@ object BlockPlacementRuleRegistrations {
     @JvmStatic
     fun register(vararg blockGroups: PlacementGroup) {
         val blockManager = MinecraftServer.getBlockManager()
+        var count = 0
 
         for (placementGroup in blockGroups) {
             val blockGroup = placementGroup.blockGroup
             for (block in blockGroup.allMatching()) {
+                count++
                 blockManager.registerBlockPlacementRule(placementGroup.createRule(block))
             }
         }
+
+        logger.info("Registered $count block placement rules")
     }
 }
