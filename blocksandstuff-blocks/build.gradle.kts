4b0d29102d49c85ad4f6c9bba1caae08c9186a2d
Nico Weber <nico.weber24@gmx.de>
2025-06-18 01:46:06 +0200

    chore: update JVM toolchain to 21 across all modules

--- blocksandstuff-blocks/build.gradle.kts
+++ blocksandstuff-blocks/build.gradle.kts
@@ -26,7 +26,7 @@ tasks.test {
 }
 
 kotlin {
-    jvmToolchain(23)
+    jvmToolchain(21)
 }
 
 publishing {
--- blocksandstuff-common/build.gradle.kts
+++ blocksandstuff-common/build.gradle.kts
@@ -23,7 +23,7 @@ tasks.test {
 }
 
 kotlin {
-    jvmToolchain(23)
+    jvmToolchain(21)
 }
 
 publishing {
