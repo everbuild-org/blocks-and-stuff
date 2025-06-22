d2eecc9bd597c8d643b3cfe44c8e6793efb09186
Christian Bergschneider <christian.bergschneider@gmx.de>
2025-06-23 00:45:44 +0200

    docs: general dokka support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.1.10" apply false
+    kotlin("jvm") version "2.1.10"
+    id("org.jetbrains.dokka") version "2.0.0"
 }
 
 group = "org.everbuild.blocksandstuff"
@@ -9,3 +10,15 @@ allprojects {
     group = rootProject.group
     version = rootProject.version
 }
+
+dependencies {
+    dokka(project(":blocksandstuff-blocks"))
+    dokka(project(":blocksandstuff-fluids"))
+    dokka(project(":blocksandstuff-common"))
+}
+
+subprojects {
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jetbrains.dokka")
+}
+
