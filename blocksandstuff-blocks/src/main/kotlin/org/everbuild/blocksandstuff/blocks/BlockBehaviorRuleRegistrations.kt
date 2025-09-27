8da3322164d317853d214c51c78d3a4c6b1eb23d
Christian Bergschneider <christian.bergschneider@gmx.de>
2025-09-27 17:25:00 +0200

    feat: add support for excluding blocks during registration

--- blocksandstuff-blocks/src/main/kotlin/org/everbuild/blocksandstuff/blocks/BlockBehaviorRuleRegistrations.kt
+++ blocksandstuff-blocks/src/main/kotlin/org/everbuild/blocksandstuff/blocks/BlockBehaviorRuleRegistrations.kt
@@ -1,6 +1,7 @@
 package org.everbuild.blocksandstuff.blocks
 
 import net.minestom.server.MinecraftServer
+import net.minestom.server.instance.block.Block
 import org.everbuild.blocksandstuff.blocks.group.VanillaBlockBehaviour
 import org.everbuild.blocksandstuff.blocks.group.behaviour.BehaviourGroup
 import org.everbuild.blocksandstuff.blocks.randomticking.getRandomTickingEventNode
@@ -13,20 +14,33 @@ object BlockBehaviorRuleRegistrations {
         MinecraftServer.getGlobalEventHandler().addChild(getRandomTickingEventNode())
     }
 
+
     @JvmStatic
     fun registerDefault() {
-        register(*VanillaBlockBehaviour.ALL.toTypedArray<BehaviourGroup>())
-        registerRandomTickingGlobally()
+        registerDefault(listOf())
     }
 
     @JvmStatic
+    fun registerDefault(exclude: List<Block>) {
+        register(exclude, *VanillaBlockBehaviour.ALL.toTypedArray<BehaviourGroup>())
+        registerRandomTickingGlobally()
+    }
+
+
+    @JvmStatic
     fun register(vararg blockGroups: BehaviourGroup) {
+        register(listOf(), *blockGroups)
+    }
+
+    @JvmStatic
+    fun register(exclude: List<Block>, vararg blockGroups: BehaviourGroup) {
         val blockManager = MinecraftServer.getBlockManager()
         var count = 0
 
         for (group in blockGroups) {
             val blockGroup = group.blockGroup
             for (block in blockGroup.allMatching()) {
+                if (exclude.contains(block)) continue
                 count++
                 val handler = group.createHandler(block)
                 blockManager.registerHandler(block.key().asString()) { handler }
