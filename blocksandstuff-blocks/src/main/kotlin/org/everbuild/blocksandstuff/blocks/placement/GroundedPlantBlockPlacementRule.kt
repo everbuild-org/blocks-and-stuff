9edc07956222c89d6cc214555c0e4a3961da67af
Nico Weber <nico.weber24@gmx.de>
2026-04-16 12:15:25 +0200

    chore: improve block placement rules, enhance Farmland and GroundedPlant behaviors, and reformat for consistency and readability

--- blocksandstuff-blocks/src/main/kotlin/org/everbuild/blocksandstuff/blocks/placement/GroundedPlantBlockPlacementRule.kt
+++ blocksandstuff-blocks/src/main/kotlin/org/everbuild/blocksandstuff/blocks/placement/GroundedPlantBlockPlacementRule.kt
@@ -1,32 +1,52 @@
 package org.everbuild.blocksandstuff.blocks.placement
 
+import net.minestom.server.coordinate.Point
 import net.minestom.server.instance.block.Block
 import net.minestom.server.instance.block.rule.BlockPlacementRule
-import net.minestom.server.registry.TagKey
 import org.everbuild.blocksandstuff.common.item.DroppedItemFactory
+import org.everbuild.blocksandstuff.common.utils.isWater
 
-class GroundedPlantBlockPlacementRule(block: Block) : BlockPlacementRule(block) {
-    private val dirtBlocks = Block.staticRegistry().getTag(TagKey.ofHash("#minecraft:dirt"))!!
-
+class GroundedPlantBlockPlacementRule(
+    block: Block,
+) : BlockPlacementRule(block) {
     override fun blockPlace(placementState: PlacementState): Block? {
-        val blockBelow = placementState.instance.getBlock(placementState.placePosition.add(0.0, -1.0, 0.0))
+        val currentBlock = placementState.instance.getBlock(placementState.placePosition)
+        val waterlogged = currentBlock.isWater().toString()
+        val finalBlock =
+            if (placementState.block.properties().containsKey("waterlogged")) {
+                placementState.block.withProperty("waterlogged", waterlogged)
+            } else {
+                placementState.block
+            }
 
-        if (dirtBlocks.contains(blockBelow)) {
-            return placementState.block
+        return if (isSupported(placementState.instance, placementState.placePosition)) {
+            finalBlock
+        } else {
+            null
         }
-        return null
-
     }
 
     override fun blockUpdate(updateState: UpdateState): Block {
-        val blockBelow = updateState.instance.getBlock(updateState.blockPosition.add(0.0, -1.0, 0.0))
-
-        if (!dirtBlocks.contains(blockBelow)) {
+        if (!isSupported(updateState.instance, updateState.blockPosition)) {
             DroppedItemFactory.maybeDrop(updateState)
             return Block.AIR
         }
-
         return updateState.currentBlock
+    }
 
+    private fun isSupported(
+        instance: Block.Getter,
+        pos: Point,
+    ): Boolean {
+        val blockBelow = instance.getBlock(pos.sub(0.0, 1.0, 0.0))
+        return blockBelow.compare(Block.DIRT) ||
+            blockBelow.compare(Block.GRASS_BLOCK) ||
+            blockBelow.compare(Block.COARSE_DIRT) ||
+            blockBelow.compare(Block.PODZOL) ||
+            blockBelow.compare(Block.ROOTED_DIRT) ||
+            blockBelow.compare(Block.MOSS_BLOCK) ||
+            blockBelow.compare(Block.FARMLAND) ||
+            blockBelow.compare(Block.MUD) ||
+            blockBelow.compare(Block.MYCELIUM)
     }
 }
--- blocksandstuff-recipes/src/main/kotlin/org/everbuild/blocksandstuff/recipes/util/InventoryButtonClickListener.kt
+++ blocksandstuff-recipes/src/main/kotlin/org/everbuild/blocksandstuff/recipes/util/InventoryButtonClickListener.kt
@@ -2,11 +2,15 @@ package org.everbuild.blocksandstuff.recipes.util
 
 import net.minestom.server.entity.Player
 import net.minestom.server.event.EventDispatcher
+import net.minestom.server.event.inventory.InventoryButtonClickEvent
 import net.minestom.server.inventory.Inventory
 import net.minestom.server.network.packet.client.play.ClientClickWindowButtonPacket
 
 object InventoryButtonClickListener {
-    fun inventoryButtonClickListener(packet: ClientClickWindowButtonPacket, player: Player) {
+    fun inventoryButtonClickListener(
+        packet: ClientClickWindowButtonPacket,
+        player: Player,
+    ) {
         val inventory = player.openInventory ?: return
         if (packet.windowId != inventory.windowId.toInt()) return
 
